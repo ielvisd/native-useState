0d677c675041c5ab21be996214883272e678cca8
Elvis Ibarra <ielvisd@gmail.com>
2023-02-05 20:21:05 -0800

    First useState example that works

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
 import { useState } from 'react'
 
+function NameList() {
+  const [list, setList] = useState(['John', 'Jane', 'Jack'])
+  const [name, setName] = useState('')
+
+  const onAddName = () => {
+    // You need to invoke the setter function to update the state, I hate this lol
+    setList([...list, name])
+    setName('')
+  }
+
+
+  return (
+    <div>
+      <ul>
+        {list.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value = {name}
+        onChange = {e => setName(e.target.value)}
+      />
+      <button
+        onClick={onAddName}
+      >
+        Add Name
+      </button>
+    </div>
+  )
+}
+
 function Counter() {
   // useState return an array, the first item is the current value and the second item is a setter function
   let [count, setCount] = useState(10)
@@ -19,6 +51,7 @@ function App() {
   return (
     <div>
       <Counter />
+      <NameList />
       <Counter />
     </div>
   )
